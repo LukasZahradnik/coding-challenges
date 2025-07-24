c34b49641cd7466c61466fa4a292911c2a5058e8
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2025-07-24 03:49:20 +0200

    Add in memory store

--- signing-service-challenge-go/persistence/inmemory.go
+++ signing-service-challenge-go/persistence/inmemory.go
@@ -1,3 +1,109 @@
 package persistence
 
-// TODO: in-memory persistence ...
+import (
+	"errors"
+	"maps"
+	"slices"
+	"sync"
+)
+
+var (
+	ErrItemExists   = errors.New("item already exists")
+	ErrItemNotFound = errors.New("item not found")
+)
+
+type Store[T any] interface {
+	Store(id string, item T) error
+	Set(id string, item T) error
+	Get(id string) (*T, error)
+	List() ([]T, error)
+	Lock(id string) error
+	Unlock(id string) error
+}
+
+type InMemoryStore[T any] struct {
+	storage map[string]T
+	locks   map[string]*sync.RWMutex
+	mu      sync.RWMutex
+}
+
+func NewInMemoryStore[T any]() *InMemoryStore[T] {
+	return &InMemoryStore[T]{
+		storage: make(map[string]T),
+		locks:   make(map[string]*sync.RWMutex),
+	}
+}
+
+func (s *InMemoryStore[T]) Store(id string, item T) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, ok := s.storage[id]; ok {
+		return ErrItemExists
+	}
+
+	s.storage[id] = item
+	s.locks[id] = &sync.RWMutex{}
+
+	return nil
+}
+
+func (s *InMemoryStore[T]) Set(id string, item T) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.storage[id] = item
+
+	return nil
+}
+
+func (s *InMemoryStore[T]) Get(id string) (*T, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	item, ok := s.storage[id]
+	if !ok {
+		return nil, ErrItemNotFound
+	}
+
+	return &item, nil
+}
+
+func (s *InMemoryStore[T]) List() ([]T, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if len(s.storage) == 0 {
+		return []T{}, nil
+	}
+
+	return slices.Collect(maps.Values(s.storage)), nil
+}
+
+func (s *InMemoryStore[T]) Lock(id string) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	lock, ok := s.locks[id]
+	if !ok {
+		return ErrItemNotFound
+	}
+
+	lock.Lock()
+
+	return nil
+}
+
+func (s *InMemoryStore[T]) Unlock(id string) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	lock, ok := s.locks[id]
+	if !ok {
+		return ErrItemNotFound
+	}
+
+	lock.Unlock()
+
+	return nil
+}
---
+++ signing-service-challenge-go/persistence/inmemory_test.go
@@ -0,0 +1,82 @@
+package persistence_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/persistence"
+	"golang.org/x/exp/slices"
+)
+
+func TestStoreItem(t *testing.T) {
+	store := persistence.NewInMemoryStore[int]()
+
+	if err := store.Store("some id", 1); err != nil {
+		t.Errorf("Storing item in store returned unexpected error: %s", err.Error())
+	}
+}
+
+func TestStoreDuplicitItem(t *testing.T) {
+	store := persistence.NewInMemoryStore[int]()
+
+	if err := store.Store("some id", 1); err != nil {
+		t.Errorf("Storing item in store returned unexpected error: %s", err.Error())
+	}
+
+	if err := store.Store("some id", 1); !errors.Is(err, persistence.ErrItemExists) {
+		t.Error("Storing item in store twice did not return expected error")
+	}
+}
+
+func TestGetNonExistingItem(t *testing.T) {
+	store := persistence.NewInMemoryStore[int]()
+
+	_, err := store.Get("invalid id")
+
+	if !errors.Is(err, persistence.ErrItemNotFound) {
+		t.Error("Getting non existent item from store did not return expected error")
+	}
+}
+
+func TestGetItem(t *testing.T) {
+	store := persistence.NewInMemoryStore[int]()
+
+	_, err := store.Get("invalid id")
+
+	if !errors.Is(err, persistence.ErrItemNotFound) {
+		t.Error("Getting non existent item from store did not return expected error")
+	}
+}
+
+func TestListItems(t *testing.T) {
+	store := persistence.NewInMemoryStore[int]()
+
+	items, err := store.List()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+
+	if len(items) != 0 {
+		t.Errorf("Unexpected size of the item list: %d, expected 0", len(items))
+	}
+
+	store.Store("some id", 1)
+	store.Store("some id", 2)
+
+	items, err = store.List()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+	}
+
+	if len(items) != 2 {
+		t.Errorf("Unexpected size of the item list: %d, expected 2", len(items))
+	}
+
+	if slices.Contains(items, 1) {
+		t.Error("Item '1' not found in the item list")
+	}
+
+	if slices.Contains(items, 2) {
+		t.Error("Item '2' not found in the item list")
+	}
+}
